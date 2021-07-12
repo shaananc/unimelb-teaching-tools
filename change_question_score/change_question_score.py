f2bc9dc6e62b83592e387d51cfc20a7188ced181
Shaanan Cohney <shaananc@gmail.com>
2021-07-12 14:18:25 +1000

    change_question_score ready for release

--- change_question_score/change_question_score.py
+++ change_question_score/change_question_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 This script allows you to change the score for students for a particular question.
+Currently it is configured to set all questions with a maximum score of 0 to 0 in each students' submission
 
 Why do this? In my exams I typically include an ungraded question in which students can express themselves.
 Tutors will not grade this question, leaving the quizes partially graded and not ready for release.
@@ -8,35 +9,33 @@ By running this script, I set the points awarded for that question to zero, enab
 
 This script is supplemented by config.ini and rubric.py, for which samples are provided.
 """
-from rubric import rubric
 from rich import print
 from rich.console import Console
-from rich.panel import Panel
 
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(".."))
+from pathlib import Path
+
+sys.path.insert(0, str(Path(os.path.realpath(__file__)).parent.parent))
 from utils import (  # pylint:disable=wrong-import-position
     submit_quiz_payload,
     get_quiz_info,
     get_users,
     get_user_info,
     get_quiz_submission_history,
+    logger,
 )
+from rubric import rubric
 
 console = Console()
 
 
 def process_submission(submission):
     """Sets the score on an individual user's submission"""
-    console.clear()
     user = get_user_info(submission["user_id"])["name"].encode("utf-8").decode("ascii")
 
-    console.print(
-        Panel(f"[bold cyan]Grading User {user}[/bold cyan]", expand=False),
-        justify="center",
-    )
+    logger.info(f"[bold cyan]Processing User {user}[/bold cyan]")
 
     submission_history = sorted(
         submission["submission_history"], key=lambda x: x["attempt"]
@@ -50,6 +49,9 @@ def process_submission(submission):
 
     for student_answer in most_recent_answers:
         question_id = student_answer["question_id"]
+        if question_id not in rubric:
+            raise KeyError(f"Rubric is missing entry for Question {question_id}")
+        # this controls which question to set to zero, change this condition as needed
         if not rubric[question_id]["total_points"]:
             q_grade = 0
             q_comment = ""
@@ -63,15 +65,15 @@ def process_submission(submission):
             }
         ]
     }
-    console.print(question_grades)
+    logger.info(question_grades)
 
     submit_quiz_payload(submission_history[0]["id"], payload)
 
 
 def main():
-    console.print("Downloading User Data...")
+    logger.info("Downloading User Data...")
     get_users()
-    console.print("Fetching Quiz Answers...")
+    logger.info("Fetching Quiz Answers...")
     quiz = get_quiz_info()
     quiz_assignment_id = quiz["assignment_id"]
     for submission in get_quiz_submission_history(quiz_assignment_id):
@@ -80,8 +82,7 @@ def main():
         except KeyboardInterrupt:
             continue
         except Exception as e:
-            # raise (e)
-            print(e)
+            raise (e)
             continue
 
 
