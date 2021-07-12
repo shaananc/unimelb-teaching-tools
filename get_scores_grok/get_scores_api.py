b787658b781baade30bfa22cd8faeaa07ef2af51
Shaanan Cohney <shaananc@gmail.com>
2021-07-12 14:55:41 +1000

    grok get scores ready for release

--- get_scores_grok/get_scores_api.py
+++ get_scores_grok/get_scores_api.py
@@ -9,7 +9,6 @@ from json import JSONDecodeError
 import requests
 from rich import print
 from cachier import cachier
-from datetime import timedelta
 import datetime
 import logging
 import os
@@ -27,10 +26,14 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(os.path.realpath(__file__)).parent.parent))
-from utils import get_truthy_config_option  # pylint:disable=wrong-import-position
+from utils import (
+    get_truthy_config_option,
+    logger,
+    cache_expiry,
+)  # pylint:disable=wrong-import-position
 
 MODULE_CONFIG_SECTION = "GROK"
-session_token = get_truthy_config_option("grok_token", MODULE_CONFIG_SECTION)
+OUTPUT_FILENAME = "scores.csv"
 course_slug = get_truthy_config_option("grok_course_slug", MODULE_CONFIG_SECTION)
 problem_suffix = get_truthy_config_option("grok_problem_suffix", MODULE_CONFIG_SECTION)
 
@@ -47,28 +50,23 @@ logging.basicConfig(
     level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
 )
 
-try:
-    os.remove("scores_api.txt")
-except Exception as e:
-    print(e)
 
-session = FuturesSession()
-
-if not session_token:
+def get_session_token():
     with contextlib.closing(webdriver.Firefox()) as driver:
         driver.get("https://groklearning.com/login/")
-        wait = ui.WebDriverWait(driver, 60)  # timeout after 10 seconds
+        wait = ui.WebDriverWait(driver, 180)  # timeout after 180 seconds
         wait.until(
             lambda driver: driver.find_elements_by_class_name("account-header-left")
         )
-        session_token = driver.get_cookie("grok_session")
+        session_token = driver.get_cookie("grok_session")["value"]
+        get_jar().set("grok_session", session_token, domain=".groklearning.com")
         logger.info(f"session_token={session_token}")
+        return session_token
 
 
 def get_jar():
     if not get_jar.jar:
         get_jar.jar = requests.cookies.RequestsCookieJar()
-        get_jar.jar.set("grok_session", session_token, domain=".groklearning.com")
     return get_jar.jar
 
 
@@ -78,6 +76,7 @@ get_jar.jar = None
 @cachier(stale_after=cache_expiry)
 def get_users():
     resp = requests.get(user_url, cookies=get_jar())
+    resp.raise_for_status()
     o = resp.json()
     print(f"Processing {len(o)} users...")
     return o
@@ -116,7 +115,7 @@ def response_hook(resp, logfile):
         logfile.write(f"{name},{stime},{submission['npassed']}\n")
 
 
-def get_submission_history(user):
+def get_submission_history(session, user):
     problem_url = f"{base_url}/user/{user}/{problem_suffix}"
     future = session.get(
         problem_url, cookies=get_jar(), headers={"User-agent": "your bot 0.1"}
@@ -124,12 +123,47 @@ def get_submission_history(user):
     return future
 
 
-futures = []
-for user in get_users():
-    futures.append(get_submission_history(user))
-    time.sleep(0.3)  # add delay to prevent rate limiting by Grok servers
+def main():
 
-with open("scores_api.txt", "w") as f:
+    try:
+        os.remove(OUTPUT_FILENAME)
+    except Exception as e:
+        pass
+
+    session = FuturesSession()
+
+    session_token = get_truthy_config_option("grok_token", MODULE_CONFIG_SECTION)
+    if not session_token:
+        logger.warning("Session token not found, launching Grok Login")
+        session_token = get_session_token()
+    else:
+        get_jar().set("grok_session", session_token, domain=".groklearning.com")
+
+    futures = []
+    users = []
+    logger.info("Getting User Details...")
+    try:
+        users = get_users()
+    except requests.exceptions.HTTPError as err:
+        if err.response.status_code == 401:
+            logger.warning("Session token was invalid, launching Grok Login")
+            session_token = get_session_token()
+            users = get_users()
+
+    logger.info(
+        "Submitting jobs for user submissions... Warning this may take some time"
+    )
     with logging_redirect_tqdm():
-        for future in tqdm(concurrent.futures.as_completed(futures)):
-            response_hook(future.result(), f)
+        for i, user in tqdm(enumerate(users)):
+            futures.append(get_submission_history(session, user))
+            if i % 10 == 0:
+                time.sleep(0.3)  # add delay to prevent rate limiting by Grok servers
+
+    logger.info("Awaiting responses...")
+    with open(OUTPUT_FILENAME, "w") as f:
+        with logging_redirect_tqdm():
+            for future in tqdm(concurrent.futures.as_completed(futures)):
+                response_hook(future.result(), f)
+
+
+main()
