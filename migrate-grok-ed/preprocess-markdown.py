b4da779a4c6278d9cb828ad96106fd01c08bb52f
Shaanan Cohney <shaananc@gmail.com>
2023-01-29 23:33:21 +1100

    early version of preprocessor

--- migrate-grok-ed/preprocess-markdown.py
+++ migrate-grok-ed/preprocess-markdown.py
@@ -4,19 +4,32 @@ import re
 import markdown
 import subprocess
 import html
+from bs4 import BeautifulSoup
+from unidecode import unidecode
+
+from itertools import chain
+from tqdm import tqdm
+
 
 # all printing should be done by rich 
 from rich import print
 
 def replace_entities(match):
-    return html.unescape(match.group(0))
+    print("HERE!")
+    unescaped = html.unescape(match.group(0))
+    unescaped.strip('\n').lstrip("\n")
+    return unescaped
 
 def main():
     # glob all markdown files in output/grok_exercises
     # for each file, read it in, and write it out again
     origin = Path('output/grok_exercises/')
-    files = [origin.glob('**/content.md'), origin.glob('**/solution_notes.md')] 
-    for f in files:
+    files = chain(origin.glob('**/content.md'), origin.glob('**/solution_notes.md'))
+    print(files)
+    for f in tqdm(files):
+        if not '4.10,11' in str(f.absolute()):
+            continue
+
         # find this regex: "path.*;lang:(.*);.*\n"
         # replace it with group 1
         # write it out again
@@ -30,6 +43,9 @@ def main():
 
                 text = text.replace("bashshell","bash")
                 text = re.sub(r'```.*lang:(.*?);.*\n', r'```\1\n\n', text)  
+                text = text.replace('''```
+
+```''','')
                 #print(text)
                 #break
             
@@ -45,11 +61,10 @@ def main():
         # convert the markdown to html
         html = markdown.markdown(text, extensions=['fenced_code'])
         # write the file to html using the same name, with a .html file extension instead of .md
-        with open(f'{str(f)[:-3]}.html', 'w') as fout:
+        with open(f.with_suffix('.html'), 'w') as fout:
             fout.write(html)
 
 
-        print(f)
 
         # get path of the current file
         cpath = Path(__file__).parent.absolute()
@@ -62,19 +77,45 @@ def main():
         #print(f'node {cpath} {fabs} {famber}')
         subprocess.run(['node', cpath, str(fabs), str(famber)])
 
-        amber_text = ""
-        with open(famber, 'r') as fin:
-            amber_text = fin.read()
+        # amber_text = ""
+        # with open(famber, 'r') as fin:
+        #     amber_text = fin.read()
 
 
 
 
-        amber_text = re.sub(r"<snippet>.*?</snippet>", replace_entities, amber_text, flags=re.DOTALL)
+        # amber_text = re.sub(r"<snippet>.*?</snippet>", replace_entities, amber_text, flags=re.DOTALL)
 
-        with open(famber, 'w') as fout:
-            fout.write(amber_text)
-
-
+        # with open(famber, 'w') as fout:
+        #     fout.write(amber_text)
 
 
-main()
+def unescape_all():
+    origin = Path('output/grok_exercises/')
+    files = chain(origin.glob('**/content.xml'))
+    for f in tqdm(files):
+        # if not '4.10,11' in str(f.absolute()):
+        #     continue
+
+        # print("Unescaping: " + str(f))
+
+        soup = ""
+        with open(f, 'r') as fin:
+            soup = BeautifulSoup(fin.read(), 'html.parser')
+
+        #print(soup)
+        for snippet in soup.find_all("snippet"):
+            if snippet.string:
+                snippet.string = snippet.string.rstrip().lstrip()
+                snippet.string = unidecode(html.unescape(snippet.string))
+            if not snippet.string:
+                snippet.decompose()
+        #         print(snippet.string)
+        #         print("HERE!")
+        # print(soup)
+
+        with open(f, 'w') as fout:
+            fout.write(str(soup))
+
+#main()
+unescape_all()
